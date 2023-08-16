77c1f907ba49cdc6515aa7231552413b149d4fd0
Dennis <dennisjcarvalho@gmail.com>
2023-08-15 23:14:14 -0300

    config: ajustado jest.config

--- jest/__tests__/sum.test.ts
+++ jest/__tests__/sum.test.ts
@@ -1,7 +1,13 @@
 import { sum } from '../src/sum';
 
 describe('Somar', () => {
-  test('deve somar dois números', () => {
+  test('deve falhar ao somar dois números', () => {
     expect(sum(2, 5)).toEqual(3);
+  });
+});
+
+describe('Somar', () => {
+  test('deve trazer o resultado correto ao somar dois números', () => {
+    expect(sum(5, 5)).toEqual(10);
   });
 });
