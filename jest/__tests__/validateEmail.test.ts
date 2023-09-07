13bf28c6c6990ada22324a8e8d4c5b81911ad452
Dennis <dennisjcarvalho@gmail.com>
2023-09-07 18:18:30 -0300

    docs: inserido  novas expreessoes regulares para validar email

--- jest/__tests__/validateEmail.test.ts
+++ jest/__tests__/validateEmail.test.ts
@@ -3,36 +3,15 @@ import { Email } from '../src/validateEmail';
 describe('Verifica se um email é válido', () => {
   let email = new Email();
 
-  test('deve verificar se o email contem letras de a - z.', () => {
-    expect(email.hasLetters('email')).toBe(true);
+  test('deve passar um email que não tenha .br ao final.', () => {
+    expect(email.validate('adrian@gmail.com')).toBe(true);
   });
 
-  test('deve verificar se o email contem numeros', () => {
-    expect(email.hasNumbers('email2')).toBe(true);
+  test('deve passar um email que tenha .br ao final', () => {
+    expect(email.validate('adrian@email.com.br')).toBe(true);
   });
 
-  test('deve verificar se há caracteres especiais', () => {
-    expect(email.hasSpecialCharacters('email-2')).toBe(true);
-    expect(email.hasSpecialCharacters('email!2')).toBe(true);
-    expect(email.hasSpecialCharacters('email#2')).toBe(true);
-    expect(email.hasSpecialCharacters('email%2')).toBe(true);
-    expect(email.hasSpecialCharacters('email&2')).toBe(true);
-    expect(email.hasSpecialCharacters('email_2')).toBe(true);
-    expect(email.hasSpecialCharacters('email.2')).toBe(true);
-  });
-
-  test('deve verificar se contem um simbolo @', () => {
-    expect(email.hasSymbolSign('email@')).toBe(true);
-    expect(email.hasSymbolSign('email')).toBe(false);
-  });
-
-  test('deve verificar se o primeiro caracter é uma letra', () => {
-    expect(email.firstCharacterIsLetter('email@')).toBe(true);
-    expect(email.firstCharacterIsLetter('1mail@')).toBe(false);
-    expect(email.firstCharacterIsLetter('.mail@')).toBe(false);
-  });
-
-  test('deve verificar se há dois pontos consecutivos ou caracteres especiais', () => {
-    expect(email.hasTwoPoints('email..@')).toBe(false);
+  test('deve passar um email que tenha um caracter especial no nome de usuario', () => {
+    expect(email.validate('adrian-2@gmail.com')).toBe(true);
   });
 });
--- jest/src/validateEmail.ts
+++ jest/src/validateEmail.ts
@@ -1,18 +1,24 @@
 export class Email {
 
-  hasLetters(email: string): boolean {
-    let regexp = /^([aA-zZ]){1,}$/gi;
-    return regexp.test(email);
-  }
+  validate(email: string): boolean {
 
-  hasNumbers(email: string): boolean {
-    let regexp = /^([aA-zZ0-9]){1,}$/gi;
-    return regexp.test(email);
-  }
+    let exp1 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([@]){1}([aA-zZ0-9]){1,}([.]){1}([aA-zZ]){3}$/, 'gi');
 
-  hasSpecialCharacters(email: string): boolean {
-    let regexp = /^([aA-zZ0-9._#\-~$&%!]){1,}$/gi;
-    return regexp.test(email);
+    let exp2 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([@]){1}([aA-zZ0-9]){1,}([.]){1}([aA-zZ]){3}([.]){0,1}([aA-zZ]){0,2}$/, 'gi');
+
+    let exp3 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([_\-.!#$%&*()_+<>]){0,1}([aA-zZ0-9]){0,}([@]){1}([aA-zZ0-9]){1,}([.]){1}([aA-zZ]){3}([.]){0,1}([aA-zZ]){0,2}$/, 'gi');
+
+    if ((exp1.test(email))
+      || (exp2.test(email))
+      || (exp3.test(email))
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+
+
+
   }
 
   hasSymbolSign(email: string): boolean {
@@ -20,8 +26,33 @@ export class Email {
     return regexp.test(email);
   }
 
-  firstCharacterIsLetter(email: string) {
+  firstCharacterIsLetter(email: string): boolean {
     let regexp = /^([aA-zZ]){1}([aA-zZ0-9._#\-~$&%!]){1,}[@]{1}$/gi;
     return regexp.test(email);
+  }
+
+  hasTwoPoints(email: string): boolean {
+    let characterSpecial: number = 0;
+
+    for (let letter of email) {
+      if (letter === '.'
+        || letter === '_'
+        || letter === '#'
+        || letter === '-'
+        || letter === '~'
+        || letter === '$'
+        || letter === '&'
+        || letter === '%'
+        || letter === '!'
+      ) {
+        characterSpecial += 1;
+      }
+    }
+
+    if (characterSpecial > 1) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
