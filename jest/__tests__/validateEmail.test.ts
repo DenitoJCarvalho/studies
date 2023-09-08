baceed1296247c3d61ab4aba3804bbf9bdb129a3
Dennis <dennisjcarvalho@gmail.com>
2023-09-08 12:33:27 -0300

    docs: finalizei os testes para validacao de email

--- jest/__tests__/validateEmail.test.ts
+++ jest/__tests__/validateEmail.test.ts
@@ -16,14 +16,34 @@ describe('Verifica se um email é válido', () => {
   });
 
   test('deve passar um email que tenha um "." dividindo o nome de usuário.', () => {
-    expect(email.validate('adria.carvalho@gmail.com')).toBe(true);
+    expect(email.validate('adrian.carvalho@gmail.com')).toBe(true);
   });
 
   test('deve passar um email que tenha um "-" dividindo o nome de usuário.', () => {
-    expect(email.validate('adria-carvalho@gmail.com')).toBe(true);
+    expect(email.validate('adrian-carvalho@gmail.com')).toBe(true);
   });
 
   test('deve passar um email que tenha um "_" dividindo o nome de usuário.', () => {
-    expect(email.validate('adria_carvalho@gmail.com')).toBe(true);
+    expect(email.validate('adrian_carvalho@gmail.com')).toBe(true);
+  });
+
+  test('deve retornar falso ao ter um caracter especial no início do nome de usuário.', () => {
+    expect(email.validate('.adrian@gmail.com')).toBe(false);
+  });
+
+  test('deve retornar falso ao ter dois caracteres especiais seguidos.', () => {
+    expect(email.validate('adrian..123@gmail.com')).toBe(false);
+  });
+
+  test('deve retornar falso ao ter um caracter especial no final do nome de usuário.', () => {
+    expect(email.validate('adrian.#@gmail.com')).toBe(false);
+  });
+
+  test('deve retornar verdadeiro ao ter um caracter especial dividindo o dominio.', () => {
+    expect(email.validate('adrian@email-provider.com.br')).toBe(true);
+  });
+
+  test('deve retornar verdadeiro ao ter um caracter especial dividindo o nome e de usuário e outro o provider', () => {
+    expect(email.validate('email.100@email-provider.com.br')).toBe(true);
   });
 });
--- jest/src/validateEmail.ts
+++ jest/src/validateEmail.ts
@@ -2,57 +2,28 @@ export class Email {
 
   validate(email: string): boolean {
 
+    //verifica email no padrão email@gmail.com
     let exp1 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([@]){1}([aA-zZ0-9]){1,}([.]){1}([aA-zZ]){3}$/, 'gi');
 
+    //verifica email no padrão email@email.com.br
     let exp2 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([@]){1}([aA-zZ0-9]){1,}([.]){1}([aA-zZ]){3}([.]){0,1}([aA-zZ]){0,2}$/, 'gi');
 
+    //verifica email no padrão email.outraparte@gmail.com ou email.outraparte@gmail.com.br 
     let exp3 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([_\-.!#$%&*()_+<>]){0,1}([aA-zZ0-9]){0,}([@]){1}([aA-zZ0-9]){1,}([.]){1}([aA-zZ]){3}([.]){0,1}([aA-zZ]){0,2}$/, 'gi');
+
+    //verifica email no padrão email@email-provider.com ou email@email-provider.com.br ou email-outraparte@email-provider.com.br
+    let exp4 = new RegExp(/^([aA-zZ]){1}([aA-zZ0-9]){1,}([_\-.!#$%&*()_+<>]){0,1}([aA-zZ0-9]){0,}([@]){1}([aA-zZ0-9]){1,}([.\-_]){0,1}([aA-zZ]){0,}([.]){1}([aA-zZ]){3}([.]){0,1}([aA-zZ]){0,2}$/, 'gi');
 
     if ((exp1.test(email))
       || (exp2.test(email))
       || (exp3.test(email))
+      || (exp4.test(email))
     ) {
       return true;
     } else {
       return false;
     }
 
-
-
   }
 
-  hasSymbolSign(email: string): boolean {
-    let regexp = /^([aA-zZ0-9._#\-~$&%!]){1,}[@]{1}$/gi;
-    return regexp.test(email);
-  }
-
-  firstCharacterIsLetter(email: string): boolean {
-    let regexp = /^([aA-zZ]){1}([aA-zZ0-9._#\-~$&%!]){1,}[@]{1}$/gi;
-    return regexp.test(email);
-  }
-
-  hasTwoPoints(email: string): boolean {
-    let characterSpecial: number = 0;
-
-    for (let letter of email) {
-      if (letter === '.'
-        || letter === '_'
-        || letter === '#'
-        || letter === '-'
-        || letter === '~'
-        || letter === '$'
-        || letter === '&'
-        || letter === '%'
-        || letter === '!'
-      ) {
-        characterSpecial += 1;
-      }
-    }
-
-    if (characterSpecial > 1) {
-      return false;
-    } else {
-      return true;
-    }
-  }
 }
